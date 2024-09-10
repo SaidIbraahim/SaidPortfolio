b77b0f0c778fd4ae701dec0f1d48a72002000081
Said Ibrahim <145333786+SaidIbraahim@users.noreply.github.com>
2024-09-10 22:42:29 +0000

    Set up Vite and react plugin, fixed dependencies issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "/styles.css";
+import "../public/styles.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
